e0a5d72b6df2b20582628e68211b3c1a50acd97b
palloc <palloc.mknet10@gmail.com>
2016-10-03 22:00:49 +0900

    add operate_csv

---
+++ operate_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+with open("data_set/kddcup.data_10_percent", "rb") as r_file:
+    with open("data_set/10_percent_kddcup.data", "w") as w_file:
+        writer = csv.writer(w_file)
+        temp = r_file.read().split("\n")
+        data = []
+        for i in temp:
+            split_data = i.split(",")
+            del split_data[1:4]
+            data.append(split_data)
+        writer.writerows(data)
+
+        
+            
+    
